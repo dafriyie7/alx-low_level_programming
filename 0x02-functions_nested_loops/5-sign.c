a3dfa6fef6d7733106fd0b715dd4bfab62b1a0fc
Daniel Afriyie <dafriyie7@gmail.com>
2023-03-22 01:02:29 +0000

    reupload

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,15 +11,16 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43_;
 		return (1);
-	} else if (n == 0)
-}
+	}
+	else if (n < 0)
+	{
+		_purchar(45);
+		return (-1);
+	}
+	else
+	{
 		_putchar(48);
 		return (0);
-	} else if (n < 0)
-	{
-		_putchar('-');
-	}
-		return (-1);
 }
